6ce7c2641c5a9b6665ae3158e5041aa2573a5810
Adam Goldberg <adamwgoldberg@gmail.com>
2014-01-19 03:07:50 -0800

    Fixed path

--- index.py
+++ index.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return """
-           <link href="/YOUR_PATH/favicon.ico" rel="icon" type="image/x-icon" />
+           <link href="/static/images/favicon.ico" rel="icon" type="image/x-icon" />
            Hello! Welcome to Adam Goldberg\'s website!'
            """
