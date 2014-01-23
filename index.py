b22f401d1463de822a581df0295fb77bc3f98bca
Adam Goldberg <adamwgoldberg@gmail.com>
2014-01-23 03:25:25 -0800

    Added base/index/404 files

--- index.py
+++ index.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+# Controllers
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404\
 
 @app.route('/')
 def hello():
     return """
-           <link href="/static/images/favicon.ico" rel="icon" type="image/x-icon" />
            Hello! Welcome to Adam Goldberg\'s website!
            Coming soon... :)
            """
